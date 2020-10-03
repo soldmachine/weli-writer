d5ba47266847d0f1640a5046f1ac08c0adb4d0d8
Thomas Szoldatits <thomas.szoldatits@gmail.com>
2020-10-03 22:25:33 +0200

    Update dependencies + Add super basic (string parser) UI

--- weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/MainActivity.kt
+++ weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/MainActivity.kt
@@ -3,7 +3,9 @@ package com.szoldapps.weli.writer
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainViewModel.someFunc()
+        mainViewModel.result.observe(this, Observer {
+            result.text = it
+        })
+        calcButton.setOnClickListener {
+            mainViewModel.calculateResult(editText.text.toString())
+        }
     }
 }
--- weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/MainViewModel.kt
+++ weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/MainViewModel.kt
@@ -1,15 +1,60 @@
 package com.szoldapps.weli.writer
 
-import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.szoldapps.weli.writer.calculation.CalculationRepository
+import com.szoldapps.weli.writer.calculation.*
+import java.lang.Exception
 
 class MainViewModel @ViewModelInject constructor(
     private val calculationRepository: CalculationRepository
 ) : ViewModel() {
 
-    fun someFunc() {
-        Log.v("TAG", "working!")
+    val result: MutableLiveData<String> = MutableLiveData("")
+
+    fun calculateResult(input: String) {
+        try {
+            val output = StringBuilder()
+            val rows = input.split("\n")
+            val players = rows[0].split(",").map { Player(it) }
+            val rounds = mutableListOf<Round>()
+            output.appendLine(players.toString())
+            rows.forEachIndexed { index, row ->
+                if (index != 0) {
+                    val values = row.split(",")
+                    output.appendLine(values.toString())
+                    rounds.add(generateRound(index, values.map { it.toInt() }, players))
+                }
+            }
+            val game = Game(rounds = rounds)
+            val calculationResult = calculationRepository.calculateResult(game)
+            result.postValue(calculationResult.payments.joinToString("\n"))
+        } catch (e: Exception) {
+            result.postValue("ERROR!")
+        }
+    }
+
+    private fun generateRound(number: Int, points: List<Int>, players: List<Player>): Round {
+        return Round(
+            number = number,
+            values = listOf(
+                RoundValue(
+                    player = players[0],
+                    points = points[0]
+                ),
+                RoundValue(
+                    player = players[1],
+                    points = points[1]
+                ),
+                RoundValue(
+                    player = players[2],
+                    points = points[2]
+                ),
+                RoundValue(
+                    player = players[3],
+                    points = points[3]
+                )
+            )
+        )
     }
 }
