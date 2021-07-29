207bf7b23e516cc54a32fa8c4642dd9d643d0b4f
Thomas Szoldatits <thomas.szoldatits@gmail.com>
2021-07-29 22:49:21 +0200

    Composed MatchlistFragment

--- weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/presentation/match_list/MatchListFragment.kt
+++ weli_writer_android/app/src/main/java/com/szoldapps/weli/writer/presentation/match_list/MatchListFragment.kt
@@ -1,115 +1,166 @@
 package com.szoldapps.weli.writer.presentation.match_list
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.composethemeadapter.MdcTheme
 import com.szoldapps.weli.writer.R
-import com.szoldapps.weli.writer.databinding.FragmentMatchListBinding
 import com.szoldapps.weli.writer.domain.Match
-import com.szoldapps.weli.writer.presentation.common.helper.viewBinding
 import com.szoldapps.weli.writer.presentation.match_list.MatchViewState.Content
 import com.szoldapps.weli.writer.presentation.match_list.MatchViewState.Error
 import com.szoldapps.weli.writer.presentation.match_list.MatchViewState.Loading
-import com.szoldapps.weli.writer.presentation.match_list.adapter.MatchListRvAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Shows a list of [Match]es
  */
 @AndroidEntryPoint
-class MatchListFragment : Fragment(R.layout.fragment_match_list) {
+class MatchListFragment : Fragment() {
 
-    private val binding by viewBinding(FragmentMatchListBinding::bind)
-
-    private val listViewModel: MatchListViewModel by viewModels()
-
-    private val matchRvAdapter = MatchListRvAdapter { matchId ->
-        findNavController().navigate(MatchListFragmentDirections.actionMatchListFragmentToMatchFragment(matchId))
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
-
-        setupToolbarAndRv()
-        listViewModel.viewState.observe(viewLifecycleOwner, ::handleViewState)
-
-        val greeting = view.findViewById<ComposeView>(R.id.greeting)
-        greeting.setContent {
-            MdcTheme {
-                Greeting()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    MatchListScaffold(
+                        onItemClickListener = { matchId ->
+                            findNavController().navigate(
+                                MatchListFragmentDirections.actionMatchListFragmentToMatchFragment(matchId)
+                            )
+                        }
+                    )
+                }
             }
         }
-    }
-
-    private fun setupToolbarAndRv() {
-        (activity as AppCompatActivity).setSupportActionBar(binding.matchToolbar)
-        binding.matchRv.apply {
-            layoutManager = LinearLayoutManager(activity)
-            adapter = matchRvAdapter
-            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        }
-    }
-
-    private fun handleViewState(viewState: MatchViewState) {
-        when (viewState) {
-            Loading,
-            Error -> Unit
-            is Content -> matchRvAdapter.refresh(viewState.matches)
-        }
-        updateVisibility(viewState)
-    }
-
-    private fun updateVisibility(viewState: MatchViewState) {
-        with(binding) {
-            matchLoadingSpinner.isVisible = viewState is Loading
-            matchErrorTv.isVisible = viewState is Error
-            matchRv.isVisible = viewState is Content
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_match, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_match_add) {
-            listViewModel.addRandomMatch()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 }
 
 @Composable
-private fun Greeting() {
+fun MatchListScaffold(
+    viewModel: MatchListViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    onItemClickListener: (Long) -> (Unit) = { },
+) {
+    Scaffold(
+        topBar = {
+            TopBar(
+                onAddItemClickListener = { viewModel.addRandomMatch() }
+            )
+        },
+        content = {
+            MatchList(
+                onItemClickListener = { matchId ->
+                    onItemClickListener.invoke(matchId)
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun TopBar(
+    onAddItemClickListener: () -> (Unit) = { },
+) {
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.match_toolbar_title)) },
+        actions = {
+            IconButton(
+                onClick = { onAddItemClickListener.invoke() },
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = stringResource(id = R.string.match_menu_title)
+                    )
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun MatchList(
+    viewModel: MatchListViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    onItemClickListener: (Long) -> (Unit) = { },
+) {
+    val viewState by viewModel.viewState.observeAsState()
+    when (val state = viewState) {
+        is Content ->
+            Column(
+                modifier = Modifier.padding(vertical = 8.dp),
+            ) {
+                state.matches.forEach { message ->
+                    MatchRow(message, onItemClickListener)
+                }
+            }
+        Loading -> Loading()
+        Error,
+        null -> Error()
+    }
+}
+
+@Composable
+fun MatchRow(
+    match: Match,
+    onItemClickListener: (Long) -> (Unit),
+) {
     Text(
-        text = "Woooohoooo Compose is stable and works!",
-        style = MaterialTheme.typography.h5,
+        text = "${match.date}, ${match.location}",
+        style = MaterialTheme.typography.body1,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 16.dp)
+            .clickable {
+                onItemClickListener.invoke(match.id)
+            }
+            .padding(horizontal = 16.dp, vertical = 12.dp)
             .wrapContentWidth(Alignment.CenterHorizontally)
+
+    )
+}
+
+@Composable
+fun Loading() {
+    Text(
+        text = "LOADING ...",
+        style = MaterialTheme.typography.h1,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentHeight(Alignment.CenterVertically)
+    )
+}
+
+@Composable
+fun Error() {
+    Text(
+        text = "ERROR ...",
+        style = MaterialTheme.typography.h1,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentHeight(Alignment.CenterVertically)
     )
 }
