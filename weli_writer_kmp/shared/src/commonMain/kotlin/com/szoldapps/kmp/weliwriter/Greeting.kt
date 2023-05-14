a0ebd81bab3c1ceebf0fa7f8e6f941c471540835
Thomas Szoldatits <thomas.szoldatits@gmail.com>
2023-05-14 13:15:41 +0200

    Remove weli_writer_kmp to prep replacement with iOS compose version

--- weli_writer_kmp/shared/src/commonMain/kotlin/com/szoldapps/kmp/weliwriter/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.szoldapps.kmp.weliwriter
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- weli_writer_kmp/shared/src/commonMain/kotlin/com/szoldapps/kmp/weliwriter/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.szoldapps.kmp.weliwriter
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
