159e5a76c45d8560ad2fb1f099c67c02b6609588
Thomas Szoldatits <thomas.szoldatits@gmail.com>
2023-06-22 17:45:45 +0200

    Add first working MatchListViewModelTest

---
+++ weli_writer_android/app/src/test/java/com/szoldapps/weli/writer/domain/FakeMatchRepository.kt
@@ -0,0 +1,17 @@
+package com.szoldapps.weli.writer.domain
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+class FakeMatchRepository : MatchRepository {
+
+    private val _matches = MutableSharedFlow<List<Match>>()
+
+    suspend fun emit(value: List<Match>) = _matches.emit(value)
+
+    override val matches: Flow<List<Match>> = _matches
+
+    override suspend fun addMatch(match: Match) {
+        TODO("Not yet implemented")
+    }
+}
--- weli_writer_android/app/src/test/java/com/szoldapps/weli/writer/presentation/match_list/MatchListViewModelTest.kt
+++ weli_writer_android/app/src/test/java/com/szoldapps/weli/writer/presentation/match_list/MatchListViewModelTest.kt
@@ -1,26 +1,41 @@
 package com.szoldapps.weli.writer.presentation.match_list
 
-import com.szoldapps.weli.writer.domain.FakeWeliRepository
+import com.szoldapps.weli.writer.common.CoroutineTestExtension
+import com.szoldapps.weli.writer.domain.FakeMatchRepository
 import com.szoldapps.weli.writer.domain.Match
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.threeten.bp.OffsetDateTime
 
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(CoroutineTestExtension::class)
 internal class MatchListViewModelTest {
 
-//    @Test
-//    fun getUiState() = runTest {
-//        // given
-//        val match = Match(id = -1, date = OffsetDateTime.now(), location = "some location")
-//        val matchList = listOf(match)
-//        val weliRepository = FakeWeliRepository(matchList)
-//
-//        // when
-//        val viewModel = MatchListViewModel(weliRepository)
-//
-//        // then
-//        println(viewModel.uiState.toList().toString())
-//        //assertEquals(MatchViewState.Content(matchList), uiState.drop(1).first())
-//    }
+    @Test
+    fun `test uiState with one match`() = runTest {
+        // given
+        val match = Match(id = -1, date = OffsetDateTime.now(), location = "some location")
+        val matchList = listOf(match)
+        val fakeMatchRepository = FakeMatchRepository()
+
+        // when
+        val viewModel = MatchListViewModel(fakeMatchRepository)
+
+        // At least one collector needed for StateFlow,
+        // because of .stateIn(SharingStarted.WhileSubscribed())
+        backgroundScope.launch(UnconfinedTestDispatcher()) {
+            viewModel.uiState.collect {}
+        }
+
+        // then
+        assertEquals(MatchViewState.Loading, viewModel.uiState.value)
+
+        fakeMatchRepository.emit(matchList)
+        assertEquals(MatchViewState.Content(matchList), viewModel.uiState.value)
+    }
 }
