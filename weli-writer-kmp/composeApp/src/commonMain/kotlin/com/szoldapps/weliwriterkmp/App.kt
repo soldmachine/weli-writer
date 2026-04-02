a2bab83a010fbb3a15678ef2c2feceb04ad06f54
Thomas Szoldatits <thomas.szoldatits@gmail.com>
2026-04-02 16:14:31 +0200

    Update Koin init

--- weli-writer-kmp/composeApp/src/commonMain/kotlin/com/szoldapps/weliwriterkmp/App.kt
+++ weli-writer-kmp/composeApp/src/commonMain/kotlin/com/szoldapps/weliwriterkmp/App.kt
@@ -6,14 +6,18 @@ import com.szoldapps.weliwriterkmp.data.db.setup.AppDatabase
 import com.szoldapps.weliwriterkmp.di.appModules
 import com.szoldapps.weliwriterkmp.presentation.navigation.WeliNavigation
 import org.koin.compose.KoinApplication
+import org.koin.dsl.koinConfiguration
 
 @Composable
 internal fun App(appDatabase: AppDatabase) = MaterialTheme {
     KoinApplication(
-        application = {
-            modules(modules = appModules(appDatabase))
+        configuration = koinConfiguration(
+            declaration = {
+                modules(modules = appModules(appDatabase))
+            }
+        ),
+        content = {
+            WeliNavigation()
         }
-    ) {
-        WeliNavigation()
-    }
+    )
 }
